15c65efd4963cebf377368b6cc6f7b8f40eae479
Blessingkeyz <umukoroblessing@gmail.com>
2022-07-07 02:19:56 +0100

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './Footer'
+import Hero from './Hero'
+import Product from './Product'
+import Subscribe from './Subscribe'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <Product />
+        <Subscribe />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
